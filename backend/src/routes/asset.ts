1705cb7a43f994ca6acae74da2bd2d44456bf3d5
Perul Jain <perul365@gmail.com>
2021-03-15 21:54:22 +0530

    add-feature

--- backend/src/routes/asset.ts
+++ backend/src/routes/asset.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authByToken } from "../middleware/auth";
 
-const {createAsset, updateLocation, getAssets, getAsset} = require('../controllers/asset')
+const {createAsset, updateLocation, getAssets, getAsset, getAssetByTime} = require('../controllers/asset')
 
 const route = Router()
 
@@ -12,5 +12,7 @@ route.patch('/asset/:id', updateLocation)
 route.get('/asset/list', authByToken, getAssets)
 
 route.get('/asset/track/:_id', authByToken, getAsset)
+
+route.get('/asset/trackbytime/:_id', authByToken, getAssetByTime)
 
 export const assetRoute = route

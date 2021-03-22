e4d89931bcdb0a5b2d3e4045115278fa635f415f
Perul Jain <perul365@gmail.com>
2021-03-22 13:04:35 +0530

    p1-requirements

---
+++ backend/src/utils/geojson.js
@@ -0,0 +1,15 @@
+var geojson = require('geojson-tools')
+
+var convert = (coordinates)=>{
+    var array = geojson.complexify(coordinates[0], 0.5)
+    return geojson.toGeoJSON(array, 'LineString')
+}
+
+var parses = (data)=>{
+    
+    data.geometry = geojson.toGeoJSON(data.coordinates[0], 'LineString')
+
+    return data
+}
+
+module.exports = [convert, parses]
